010b7ab063bacc8060d8d51255ef5807078d8010
hexianWeb <2387213640@qq.com>
2023-03-16 21:52:07 +0800

    feat: 新增 alias的别名设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': resolve('src'), // 源码根目录
+      '@img': resolve('src/assets/img'), // 图片
+      '@less': resolve('src/assets/less'), // 预处理器
+      '@libs': resolve('src/libs'), // 本地库
+      '@plugins': resolve('src/plugins'), // 本地插件
+      '@cp': resolve('src/components'), // 公共组件
+      '@views': resolve('src/views'), // 路由组件
+      '@store': resolve('src/stores') // pinia组件
+    }
+  }
+});
