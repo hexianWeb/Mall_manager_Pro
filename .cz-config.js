11a69a5591d665e696ec5a48b79e4b666e8a424a
hexianWeb <2387213640@qq.com>
2023-03-19 20:26:05 +0800

    :sparkles: feat(custom): 新增 husky 与 commit 提交规范

---
+++ .cz-config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  // 可选类型
+  types: [
+    {
+      value: ':sparkles: feat',
+      name: '✨ feat:      新功能'
+    },
+    {
+      value: ':bug: fix',
+      name: '🐛 fix:      修复'
+    },
+    {
+      value: ':memo: docs',
+      name: '📝 docs:      文档变更'
+    },
+    {
+      value: ':lipstick: style',
+
+      name: '💄 style:     代码格式(不影响代码运行的变动)'
+    },
+    {
+      value: ':recycle: refactor',
+
+      name: '♻️  refactor:    重构 (既不增加feature, 也不是修复bug)'
+    },
+    {
+      value: ':zap: perf',
+      name: '⚡️ perf:      性能优化'
+    },
+    {
+      value: ':white_check_mark: test',
+      name: '✅ test:      增加测试'
+    },
+    {
+      value: ':wrench: chore',
+      name: '🔧 chore:     构建过程或辅助工具的变动'
+    },
+    {
+      value: ':rewind: revert',
+      name: '⏪ revert:     回退'
+    },
+    {
+      value: ':rocket: build',
+      name: '🚀 build:     打包'
+    }
+  ],
+
+  // 步骤
+
+  messages: {
+    type: '请选择提交的类型：',
+    customScope: '情输入修改的范围(可选)',
+    subject: '请简要描述提交(必填)',
+    body: '请输入详细描述(可选)',
+    footer: '请输入要关闭的issus(可选)',
+    confirmCommit: '确认要使用以上信息提交？(y/n)'
+  },
+  // 默认长度72
+  subjectLimit: 72
+};
---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# 0.0.0 (2023-03-19)
+
+### Features
+
+- 新增 集成 prettier ([801bceb](https://github.com/hexianWeb/HeXianBlog/commit/801bceb64957b8a9bb573b8e2d6201a23cad6196))
+- 新增 新增编辑器规范 ([e1687c0](https://github.com/hexianWeb/HeXianBlog/commit/e1687c08d76ccd5441288b162cfe2b2722c0235d))
+- 新增 引入 eslint ([8ae443b](https://github.com/hexianWeb/HeXianBlog/commit/8ae443b17ac4c655c18ac2939f64b399eec669b8))
+- 新增 alias 的别名设置 ([010b7ab](https://github.com/hexianWeb/HeXianBlog/commit/010b7ab063bacc8060d8d51255ef5807078d8010))
