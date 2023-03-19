b817d3b94b7a79fa17e1b5a8154181c652a042b1
hexianWeb <2387213640@qq.com>
2023-03-19 20:27:17 +0800

    :bug: fix(custom): 修改commit验证模板 启用自定义验证

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+// 如果是使用cz-customizable适配器做了破坏Angular风格的提交说明配置，
+// 那么不能使用**@commitlint/config-conventional**规则进行提交说明校验，
+// 可以使用commitlint-config-cz对定制化提交说明进行校验。
+
+// module.exports = { extends: ['@commitlint/config-conventional'] };
+
+module.exports = {
+  extends: ['cz']
+};
