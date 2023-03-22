4f0cb0065136ddef5066a8d10dd9572419d70395
hexianWeb <2387213640@qq.com>
2023-03-22 22:32:11 +0800

    :sparkles: feat(custom): 新增 element-plus 与 Vue api的自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import { resolve } from 'path';
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    // 自动按需引入 vue\vue-router\pinia 等的 api
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      resolvers: [ElementPlusResolver()],
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: true // Default `false`
+      }
+    }),
+    // 自动按需引入 第三方的组件库组件 和 我们自定义的组件
+    Components({
+      // dirs 指定组件所在位置，默认为 src/components
+      // 可以让我们使用自己定义组件的时候免去 import 的麻烦
+      dirs: ['src/components/'],
+      // 配置需要将哪些后缀类型的文件进行自动按需引入
+      extensions: ['vue', 'md'],
+      // 解析的 UI 组件库，这里以 Element Plus 和 Ant Design Vue 为例
+      resolvers: [ElementPlusResolver()]
+    }),
+    vue()
+  ],
   resolve: {
     alias: {
       '@': resolve('src'), // 源码根目录
