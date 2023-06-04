2bb4e1e77eb014574b9dcf2f47768894f40c0650
hexianWeb <2387213640@qq.com>
2023-06-04 20:45:32 +0800

    :sparkles: feat(custom): 新增部分常量定义

--- src/constants/cache_keys.ts
+++ src/constants/cache_keys.ts
@@ -1,4 +1,26 @@
+// 服务与 LOCALSTROAGE
 export const TOKEN_KEY = 'TOKEN_KEY';
+
+// 服务与 LOCALSTROAGE
 export const USER_INFO_KEY = 'USER_INFO_KEY';
+
+// 服务与 LOCALSTROAGE
 export const USER_MENUS_KEY = 'USER_MENUS_KEY';
+
+// 服务与 LOCALSTROAGE
 export const USER_PERMISSION_KEY = 'USER_PERMISSION_KEY';
+
+// 首页地址（默认）
+export const HOME_URL: string = '/main';
+
+// 登录页地址（默认）
+export const LOGIN_URL: string = '/login';
+
+// 路由白名单地址（必须是本地存在的路由 staticRouter.ts 中）
+export const ROUTER_WHITE_LIST: string[] = ['/500'];
+
+// 高德地图 key
+export const AMAP_MAP_KEY: string = '';
+
+// 百度地图 key
+export const BAIDU_MAP_KEY: string = '';
